b48541b0df4bb24cfe90672a83aa5db9e123b34e
Adrian Lara <adrianlara203@gmail.com>
2022-10-11 19:09:44 -0500

    first commit

---
+++ astro-site/src/links.js
@@ -0,0 +1,39 @@
+export const socialLinks = [
+    {
+        title: 'Instagram',
+        icon: 'mdi:instagram',
+        url: 'https://www.instagram.com/adrian1lara/',
+    },
+    {
+        title: 'LinkedIn',
+        icon: 'mdi:linkedin',
+        url: 'https://www.linkedin.com/in/adrian-lara-441b70243/',
+    },
+    {
+        title: 'Youtube',
+        icon: 'mdi:youtube',
+        url: 'https://www.facebook.com/',
+    },
+    {
+        title: 'Github',
+        icon: 'mdi:github',
+        url: 'https://github.com/SuperAang',
+    }
+]
+
+
+
+export const links = [
+    {
+        text: 'Home',
+        url: '/',
+    },
+    {
+        text:'About',
+        url: '/about',
+    },
+    {
+        text: 'Uses',
+        url: '/uses',
+    }
+]
